72642be836713c11ca78efce4d2a76924c333343
luizinhohg <loizhg96@gmail.com>
2022-07-28 21:29:50 -0300

    up

--- CadastrarPessoa.cs
+++ CadastrarPessoa.cs
@@ -65,7 +65,7 @@ namespace Sql
         string naturalidade)
         {
             // comando sql
-            cmd.CommandText = @"update Pessoa set Nome = '@nome', Cpf = '@cpf', Rg = '@rg', Naturalidade = '@naturalidade', DataNascimento = '@data_nascimento' where id = 2;";
+            cmd.CommandText = @"update Pessoa set Nome = @nome, Cpf = @cpf, Rg = @rg, DataNascimento = @data_nascimento, Naturalidade = @naturalidade where id = 1;";
             //cmd.CommandText = @"UPDATE Pessoa SET Nome = @nome, Cpf = @cpf, Naturalidade = @naturalidade, DataNascimento = @data_nascimento, Naturalidade = @naturalidade where id = 2";
 
 

0384aaff58367a031a9a496effef28a296389baf
luizinhohg <loizhg96@gmail.com>
2022-07-28 21:26:53 -0300

    Up

--- CadastrarPessoa.cs
+++ CadastrarPessoa.cs
@@ -14,17 +14,61 @@ namespace Sql
         public String mensagem = "";
         List<Pessoa> pessoas = new List<Pessoa>();
 
-        public CadastrarPessoa() { }
+        public CadastrarPessoa()
+        {
+
+        }
 
         public CadastrarPessoa(
         string nome,
         string cpf,
         string rg,
-        string data_nascimento,
+        DateTime data_nascimento,
         string naturalidade)
         {
             // comando sql
             cmd.CommandText = "insert into Pessoa (Nome, Cpf, Rg, DataNascimento, Naturalidade) values (@nome, @cpf, @rg, @data_nascimento, @naturalidade)";
+
+            using (var sql = new SqlConnection())
+            {
+                //parametros
+                cmd.Parameters.AddWithValue("@nome", nome);
+                cmd.Parameters.AddWithValue("@cpf", cpf);
+                cmd.Parameters.AddWithValue("@rg", rg);
+                cmd.Parameters.AddWithValue("@data_nascimento", data_nascimento);
+                cmd.Parameters.AddWithValue("@naturalidade", naturalidade);
+
+                //conectar com banco
+                try
+                {
+                    //conexao
+                    cmd.Connection = conexao.conectar();
+                    //executar comando
+                    cmd.ExecuteNonQuery();
+                    //desconectar
+                    conexao.desconectar();
+                    //mensagem de erro
+                    this.mensagem = "Cadastrado!";
+                }
+                catch (Exception)
+                {
+                    this.mensagem = "Erro!";
+                }
+            }
+        }
+
+        public void AtualizarPessoa(
+        string nome,
+        string cpf,
+        string rg,
+        DateTime data_nascimento,
+        string naturalidade)
+        {
+            // comando sql
+            cmd.CommandText = @"update Pessoa set Nome = '@nome', Cpf = '@cpf', Rg = '@rg', Naturalidade = '@naturalidade', DataNascimento = '@data_nascimento' where id = 2;";
+            //cmd.CommandText = @"UPDATE Pessoa SET Nome = @nome, Cpf = @cpf, Naturalidade = @naturalidade, DataNascimento = @data_nascimento, Naturalidade = @naturalidade where id = 2";
+
+
 
             using (var sql = new SqlConnection())
             {
@@ -98,6 +142,11 @@ namespace Sql
                 mensagem = "Erro!";
             }
         }
+
+
+
+
+
     }
 
 }
--- Program.cs
+++ Program.cs
@@ -17,26 +17,30 @@ namespace Sql
             DateTime data_nascimento;
             string naturalidade;
 
-            //Console.WriteLine("digite o nome : ");
-            //nome = Convert.ToString(Console.ReadLine());
-            //Console.WriteLine("digite o cpf : ");
-            //cpf = Convert.ToString(Console.ReadLine());
-            //Console.WriteLine("digite o rg : ");
-            //rg = Convert.ToString(Console.ReadLine());
-            //Console.WriteLine("digite a data de nascimento : ");
-            //data_nascimento = Convert.ToString(Console.ReadLine());
-            //Console.WriteLine("digite o naturalidade : ");
-            //naturalidade = Convert.ToString(Console.ReadLine());
+            
 
             //CadastrarPessoa cadastrar = new CadastrarPessoa(nome, cpf, rg, data_nascimento, naturalidade);
 
             CadastrarPessoa listar = new CadastrarPessoa();
             listar.ListarPessoas();
 
+            Console.WriteLine("digite o nome : ");
+            nome = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("digite o cpf : ");
+            cpf = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("digite o rg : ");
+            rg = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("digite a data de nascimento : ");
+            data_nascimento = Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine("digite o naturalidade : ");
+            naturalidade = Convert.ToString(Console.ReadLine());
+
+            CadastrarPessoa atualizar = new CadastrarPessoa();
+            atualizar.AtualizarPessoa(nome, cpf, rg, data_nascimento, naturalidade);
 
 
 
-
+            listar.ListarPessoas();
 
 
         }
